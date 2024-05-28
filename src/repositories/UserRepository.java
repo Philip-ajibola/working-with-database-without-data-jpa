9d47900398afed5d7448bb9ef377951d65355f92
philip <ajibolaphilip10@gmail.com>
2024-05-28 15:01:26 +0100

    created a db package for the so the repository can have acess to the connection

--- src/repositories/UserRepository.java
+++ src/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package repositories;
 import org.bobbysStore.User;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @SuppressWarnings("all")
 public class UserRepository {
 
@@ -18,14 +22,10 @@ public class UserRepository {
     }
 
     public User saveUser(User user) {
-        String getIdSqlStatement = "select  count(*) from users";
         String sql = "insert into users (id,wallet_id)values (?, ?)";
         try(Connection connection = connect()){
-            PreparedStatement preparedStatement = connection.prepareStatement(getIdSqlStatement);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            long currentId = resultSet.getLong(1);
-            preparedStatement = connection.prepareStatement(sql);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            long currentId = generateId();
             preparedStatement.setLong(1,currentId+1);
             preparedStatement.setObject(2,user.getWalletId());
             preparedStatement.execute();
@@ -36,10 +36,12 @@ public class UserRepository {
         }
 
     }
-    private User getUserBy(Long id){
+    public User getUserBy(Long id){
         String sql = "select * from users where id=?";
+
         try(Connection conn = connect()){
           var preparedStatement = conn.prepareStatement(sql);
+
           preparedStatement.setLong(1,id);
           var resultSet = preparedStatement.executeQuery();
           resultSet.next();
@@ -51,18 +53,83 @@ public class UserRepository {
           return user;
         }catch(SQLException e){
             System.err.println(e.getMessage());
-            throw new RuntimeException("Failed To Connect to database");
         }
-
+        return null;
     }
-    public User updateuser(Long userId, Long walletId){
+    public User updateUser(Long userId, Long walletId){
         try(Connection connection = connect()){
             String sql = "UPDATE users SET wallet_id = ? WHERE id = ? ";
-            connection.prepareStatement(sql);
-        }catch(SQLException exception){
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1,walletId);
+            preparedStatement.setLong(2,userId);
+            preparedStatement.executeUpdate();
+            return getUserBy(userId);
 
+        }catch(SQLException exception){
+            throw new RuntimeException(exception.getMessage());
         }
     }
 
 
+    public void deleteUser(Long userId) {
+        try(Connection connection = connect()){
+            String sql = "DELETE FROM users WHERE id =?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1,userId);
+            preparedStatement.execute();
+        }catch(SQLException exception){
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
+
+    public long count() {
+        String getIdSqlStatement = "select  count(*) from users";
+        String sql = "insert into users (id,wallet_id)values (?, ?)";
+        try(Connection connection = connect()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(getIdSqlStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            return resultSet.getLong(1);
+        }catch (SQLException exception){
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
+    private long generateId(){
+        try(Connection conn = connect()){
+            String sql = "SELECT max(id) FROM users";
+            var statement = conn.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            Long lastIdGenerated = resultSet.getLong(1);
+            return lastIdGenerated + 1;
+        }catch (SQLException exception){
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
+
+    public Optional<User> findUserBy(Long userId) {
+        User user = getUserBy(userId);
+        if(user == null) throw new RuntimeException("User not found");
+        return Optional.of(user);
+    }
+    public List<User> findAll(){
+        try(Connection connection = connect()){
+            String sql = "SELECT * FROM users";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            return extractUsersFrom(resultSet);
+        }catch (SQLException exception){
+            return null;
+        }
+    }
+    private List<User> extractUsersFrom(ResultSet resultSet)throws  SQLException{
+        List<User> users = new ArrayList<>();
+        while(resultSet.next()){
+            User user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setWalletId(resultSet.getLong("wallet_id"));
+            users.add(user);
+        }
+        return users;
+    }
 }
--- test/repositories/UserRepositoryTest.java
+++ test/repositories/UserRepositoryTest.java
@@ -3,6 +3,8 @@ package repositories;
 import org.bobbysStore.User;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRepositoryTest {
@@ -14,5 +16,29 @@ class UserRepositoryTest {
 //        user.setId(1l);
         User savedUser = userRepository.saveUser(user);
         assertNotNull(savedUser);
+    }
+    @Test
+    public void testUpdateUser(){
+        Long userId = 2L;
+        Long walletId = 3L;
+        User user =userRepository.updateUser(userId, walletId);
+        assertEquals(3L,user.getWalletId());
+    }
+    @Test
+    public void testDeleteUser(){
+        Long userId = 1L;
+        userRepository.deleteUser(userId);
+        assertEquals(8L,userRepository.count());
+    }
+    @Test
+    public void testDeleteUser1(){
+        Long userId = 2L;
+        userRepository.deleteUser(userId);
+        assertThrows(RuntimeException.class,()->userRepository.findUserBy(userId));
+    }
+    @Test
+    public void testFindAll(){
+        List<User> users = userRepository.findAll();
+        assertEquals(9,users.size());
     }
 }
